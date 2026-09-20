765568944ce9ff4b2eddf1bb42c12ea827b19269
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-09-20 18:43:02 -0300

    fix: correct OpenAPI Contact import

--- backend/src/main/java/com/jucelio/portfolio/config/OpenApiConfig.java
+++ backend/src/main/java/com/jucelio/portfolio/config/OpenApiConfig.java
@@ -1,6 +1,6 @@
 package com.jucelio.portfolio.config;
 
-import io.swagger.v3.oas.models.Contact;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
