61041c4fa32baa89a44fec2896a50a8e5390fe62
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-09-20 19:33:34 -0300

    fix: include legacy controller in WebMvcTest

--- backend/src/test/java/com/jucelio/portfolio/controller/PortfolioControllerTest.java
+++ backend/src/test/java/com/jucelio/portfolio/controller/PortfolioControllerTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(PortfolioController.class)
+@WebMvcTest({PortfolioController.class, LegacyPortfolioController.class})
 class PortfolioControllerTest {
 
     @Autowired
